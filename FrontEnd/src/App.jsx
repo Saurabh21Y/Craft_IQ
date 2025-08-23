dfdd60f4d2053555e75a1103725567eb1ecf52fb
Saurabh21Y <saurabhsvt030205@gmail.com>
2025-08-24 04:24:33 +0530

    Your commit message here

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
 
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Layout from './Pages/Layout'
 function App() {
 
   return (
     <div>
-      <p>Hello</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+
+        </Route>
+      </Routes>
     </div>
   )
 }
+
 
 export default App
