148563b338e4ad623ac96f09e1d2d75d865d3fd4
Saurabh21Y <saurabhsvt030205@gmail.com>
2025-09-15 00:05:09 +0530

    Frontend complete

--- FrontEnd/src/Pages/BlogTitle.jsx
+++ FrontEnd/src/Pages/BlogTitle.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Hash, Sparkles } from 'lucide-react'
 
 const BlogTitle = () => {
+  const blogCategories = [
+    'General',
+    'Technology',
+    'Business',
+    'Health',
+    'Lifestyle',
+    'Education',
+    'Travel',
+    'Food',
+  ]
+
+  const [selectedCategory, setSelectedCategory] = useState('General')
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>BlogTitle</h1>
+    <div className="h-full overflow-y-auto p-4 flex flex-row items-start flex-wrap gap-4 text-slate-700">
+      {/* Left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        {/* 1st div */}
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#8E37EB]" />
+          <h1 className="text-xl font-semibold">AI Title Generator</h1>
+        </div>
+
+        <p className="mt-6 text-sm font-medium">Keyword</p>
+        {/* 1st input box */}
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type="text"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="The future of artificial intelligence is..."
+          required
+        />
+
+        <p className="mt-4 text-sm font-medium">Category</p>
+        {/* div 2 */}
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-[82%]">
+          {blogCategories.map((item) => (
+            <span
+              onClick={() => setSelectedCategory(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedCategory === item
+                  ? 'bg-purple-50 text-purple-700'
+                  : 'text-gray-500 border-gray-300'
+              }`}
+              key={item}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+
+        <br />
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Hash className="w-5" />
+          Generate Article
+        </button>
+      </form>
+
+      {/* Right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+
+        {/* 1st div */}
+        <div className="flex items-center gap-3">
+          <Hash className="w-5 h-5 text-[#8E37EB]" />
+          <h1 className="text-xl font-semibold">Generated titles</h1>
+        </div>
+
+          {/* 2nd div */}
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Hash className="w-9 h-9" />
+            <p>Enter a topic and click “Generate Article” to get started</p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- FrontEnd/src/Pages/RemoveObject.jsx
+++ FrontEnd/src/Pages/RemoveObject.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { Scissors, Sparkle } from 'lucide-react'
+import React, { useState } from 'react'
 
 const RemoveObject = () => {
+
+  const[input, setInput] = useState('');
+  const [object, setObject] = useState('');
+
+  const onSubmitHandler = async (e)=>{
+  e.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>RemoveObject</h1>
+    <div className='h-full overflow-y-auto p-4 flex flex-row items-start flex-wrap gap-4 text-slate-700'>
+
+      {/* Left col */}
+
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        {/* 1st div */}
+        <div className='flex items-center gap-3'>
+          <Sparkle className='w-6 text-[#4A7AFF]' />
+          <h1 className='text-xl font-semibold'>Remove Object</h1>
+        </div>
+{/* ----------------------------------------------- */}
+        <p className='mt-6 text-sm font-medium'>Upload Image</p>
+        {/* 1st input box */}
+        <input
+        onChange={(e) => setInput(e.target.files[0])}
+        accept='image/*'
+        type="file" 
+        className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'
+        required
+        />
+{/* ----------------------------------------------- */}
+        <p className='mt-6 text-sm font-medium'>Describe what you want to remove</p>
+        {/* 2nd input box */}
+        <textarea
+        onChange={(e) => setObject(e.target.value)}
+        value={object}
+        rows={4}
+        className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+        placeholder="e.g., watch or spoon , Only single object name is allowed"
+        required
+        />
+        {/* button */}
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          <Scissors className='w-5' />
+          Remove Object
+        </button>
+
+
+      </form>
+
+      {/* Right col */}
+
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+          {/* 1st div */}
+          <div className='flex items-center gap-3'>
+            <Scissors className='w-5 h-5 text-[#4a7aff]'/>
+            <h1 className='text-xl font-semibold' >Processed Image</h1>
+          </div>
+
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+I
+              <Scissors className='w-9 h-9' />
+              <p>Upload an image and click "Remove Object" to get started</p>
+            </div>
+          </div>
+      </div>
+
     </div>
   )
 }
