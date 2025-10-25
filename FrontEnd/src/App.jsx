617a1bb9a2196472d98dbc40a5405bcac4eeaa9b
Saurabh21Y <saurabhsvt030205@gmail.com>
2025-10-25 07:09:27 +0530

    FInal commit

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,8 @@
-
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Layout from './Pages/Layout'
-import DashBoard from './Pages/DashBoard'
+import Dashboard from './Pages/DashBoard'
 import WriteArticle from './Pages/WriteArticle'
 import BlogTitle from './Pages/BlogTitle'
 import GenerateImages from './Pages/GenerateImages'
@@ -11,26 +10,29 @@ import RemoveBackground from './Pages/RemoveBackground'
 import RemoveObject from './Pages/RemoveObject'
 import ReviewResume from './Pages/ReviewResume'
 import Community from './Pages/Community'
-function App() {
+// import { useAuth } from '@clerk/clerk-react'
+import { Toaster } from 'react-hot-toast'
 
+const App = () => {
+  
   return (
     <div>
+      <Toaster />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ai" element={<Layout />}>
-        <Route index element={<DashBoard />} />
-        <Route path="Write-Article" element={<WriteArticle />} />
-        <Route path="Blog-Titles" element={<BlogTitle />} />
-        <Route path="Generate-Images" element={<GenerateImages />} />
-        <Route path="Remove-Background" element={<RemoveBackground />} />
-        <Route path="Remove-Object" element={<RemoveObject />} />
-        <Route path="Review-Resume" element={<ReviewResume />} />
-        <Route path="Community" element={<Community />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitle />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
         </Route>
       </Routes>
     </div>
   )
 }
-
 
 export default App
--- FrontEnd/src/Pages/DashBoard.jsx
+++ FrontEnd/src/Pages/DashBoard.jsx
@@ -1,60 +1,84 @@
 import React, { useEffect, useState } from 'react'
-import { dummyCreationData } from '../assets/assets'
-import { Gem, Sparkles } from 'lucide-react';
-import { Protect } from '@clerk/clerk-react';
-import CreationItem from '../Components/CreationItem';
+import { Gem, Sparkles } from 'lucide-react'
+import { Protect, useAuth } from '@clerk/clerk-react'
+import CreationItem from '../components/CreationItem'
+import axios from 'axios'
+import toast from 'react-hot-toast';
 
-const DashBoard = () => {
-  const [creations, setCreations] = useState([]);
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
-  useEffect(() => {
-    setCreations(dummyCreationData);
-  }, []);
+const Dashboard = () => {
 
+  const [creations, setCreations] = useState([])
+  const [loading, setLoading] = useState(true)
+  const {getToken} = useAuth()
+
+  const getDashboardData = async ()=>{
+    try {
+      const {data} = await axios.post('/api/user/get-user-creations',  {headers: {Authorization: `Bearer ${await getToken()}`}})
+      if(data.success){
+        setCreations(data.creations)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error)
+    }
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    getDashboardData()
+  },[])
   return (
-    <div className='h-full overflow-y-auto p-6'>   {/* yaha scroll bar se problem ho raha hai to scroll ko auto kr rahe hai  className='h-full overflow-y-scroll p-6' */}
-      <div className="flex flex-row gap-6">
-        {/* Total card creation */}
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
 
-        <div className='flex justify-between item-center w-100 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border-gray-200'>
           <div className='text-slate-600'>
-            <p className='text-sm'>Total Creation</p>
-            <h2 className='font-semibold text-xl'>{creations.length}</h2>
+            <p className='text-sm'>Total Creations</p>
+            <h2 className='text-xl font-semibold'>{creations.length}</h2>
           </div>
-          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex items-center justify-center'>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
             <Sparkles className='w-5 text-white'/>
           </div>
         </div>
-        {/* Plan status */}
-        <div className='flex justify-between item-center w-100 p-4 px-6 bg-white rounded-xl border border-gray-200'>
 
+
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border-gray-200'>
           <div className='text-slate-600'>
             <p className='text-sm'>Active Plan</p>
-            <h2 className='font-semibold text-xl'>
-              <Protect plan='subscription' fallback='Free'>Premium</Protect>
+            <h2 className='text-xl font-semibold'>
+              <Protect plan='subscription' fallback='Free'>Premium</Protect> Plan
             </h2>
           </div>
-
-          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex items-center justify-center'>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
             <Gem className='w-5 text-white'/>
           </div>
-          
         </div>
-
       </div>
 
-    {/* My Creations */}
-    <div className='space-y-3'>
-      <p className='mt-6 mb-4'>My Creations</p>
       {
-        creations.map((item)=> < CreationItem key={item.id} item={item}/>)
+        loading ? 
+        (
+          <div className='flex justify-center items-center h-3/4'>
+            <div className='w-10 h-10 my-1 rounded-full border-3 border-purple-500 border-t-transparent animate-spin'></div>
+          </div>
+        ) : 
+        (
+
+      <div className='space-y-3'>
+        <p className='mt-6 mb-4'>Recent Creations</p>
+        {
+          creations.map((item)=> <CreationItem key={item.id} item={item}/>)
+        } 
+      </div>
+        )
+        
       }
     </div>
-
-    </div>
-
-
   )
 }
 
-export default DashBoard
+export default Dashboard
